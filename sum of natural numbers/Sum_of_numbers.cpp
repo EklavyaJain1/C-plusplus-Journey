9236c4e8545c35f134c721e767e2e716e16cd3d7
Eklavya Jain <ej6980@srmist.edu.in>
2025-12-16 15:31:51 +0530

    Added sum of natural numbers program

---
+++ sum of natural numbers/Sum_of_numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum;
+    cout<<"Enter n number:";
+    cin>>n;
+    sum=n*(n+1)/2;
+    cout<<"The resultant sum: "<<sum<<endl;
+    return 0;
+}
